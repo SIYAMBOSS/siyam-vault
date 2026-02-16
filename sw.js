f3b58ce932048db61f57dd133ac64fbcd01f19db
Abu Saleh Mohammade Siyam <sadaf245sz@gmail.com>
2026-02-17 00:46:49 +0600

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'zeroday-v4';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './logo.png', './style.css', './script.js'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
