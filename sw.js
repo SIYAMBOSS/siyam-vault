76cd7c34c3220fb49c8215f01dc01c2842127704
Abu Saleh Mohammade Siyam <sadaf245sz@gmail.com>
2026-02-17 00:16:37 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'zeroday-cache-v1';
+const CACHE = 'zeroday-v4';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './logo.png', './manifest.json'])));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './logo.png', './style.css', './script.js'])));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
